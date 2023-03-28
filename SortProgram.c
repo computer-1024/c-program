2f95b917675813e94cdd07f6a0773b8b665326e0
lkk <kscrpt@163.com>
2023-03-28 23:04:40 +0800

    第二次提交

--- SortProgram.c
+++ SortProgram.c
@@ -38,50 +38,54 @@ void InertSort(int *arr, int n)
 }
 
 //选择排序函数
-void SelectionSort(int *arr,int n)
+void SelectionSort(int *arr, int n)
 {
-    int i,j,min_idx;
-    for(i=0;i<n;i++)
+    int i, j, min_idx;
+    for (i = 0; i < n; i++)
     {
-        min_idx=i;
-        for(j=i+1;j<n;j++)
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
         {
-            if(arr[j]<arr[min_idx])
+            if (arr[j] < arr[min_idx])
             {
-                min_idx=j;
+                min_idx = j;
             }
         }
 
-        int temp=arr[min_idx];
-        arr[min_idx]=arr[i];
-        arr[i]=temp;
-
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
     }
 }
 
-
 // 交换数组中的两个元素
-void swap(int *arr, int i, int j) {
+void swap(int *arr, int i, int j)
+{
     int temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
 }
 
 // 分治函数
-int partition(int *arr, int low, int high) {
+int partition(int *arr, int low, int high)
+{
     // 取第一个元素作为基准元素
     int pivot = arr[low];
     int i = low + 1, j = high;
-    while (1) {
+    while (1)
+    {
         // 从左往右找到第一个大于等于基准元素的元素
-        while (i <= j && arr[i] < pivot) {
+        while (i <= j && arr[i] < pivot)
+        {
             i++;
         }
         // 从右往左找到第一个小于等于基准元素的元素
-        while (i <= j && arr[j] > pivot) {
+        while (i <= j && arr[j] > pivot)
+        {
             j--;
         }
-        if (i > j) {
+        if (i > j)
+        {
             break;
         }
         // 交换左右两个元素
@@ -96,20 +100,18 @@ int partition(int *arr, int low, int high) {
 }
 
 // 快速排序函数
-void QuickSort(int *arr,int low,int high)
+void QuickSort(int *arr, int low, int high)
 {
-        if (low < high) {
+    if (low < high)
+    {
         int pivotIndex = partition(arr, low, high); // 分治
-        QuickSort(arr, low, pivotIndex - 1); // 对左半部分进行快速排序
-        QuickSort(arr, pivotIndex + 1, high); // 对右半部分进行快速排序
+        QuickSort(arr, low, pivotIndex - 1);        // 对左半部分进行快速排序
+        QuickSort(arr, pivotIndex + 1, high);       // 对右半部分进行快速排序
     }
-
 }
 
-
-void MergeSort(int *arr,int left ,int right)
+void MergeSort(int *arr, int left, int right)
 {
-    
 }
 
 int main()
@@ -132,7 +134,7 @@ int main()
     // BubbleSort(intarray, n);
     // InertSort(intarray, n);
     // SelectionSort(intarray,n);
-    QuickSort(intarray,0,n-1);
+    QuickSort(intarray, 0, n - 1);
     printf("sort print\n");
 
     for (i = 0; i < n; i++)
